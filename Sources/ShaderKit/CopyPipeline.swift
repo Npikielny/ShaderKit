e3351fc357e3db1d3b7d42514019b7be742df9dd
Noah Pikielny <np299@cornell.edu>
2022-06-12 00:39:30 -0400

    Deprecating old code

--- Sources/ShaderKit/CopyPipeline.swift
+++ Sources/ShaderKit/CopyPipeline.swift
@@ -1,84 +1,84 @@
+////
+////  CopyPipeline.swift
+////  
+////
+////  Created by Noah Pikielny on 5/22/22.
+////
 //
-//  CopyPipeline.swift
-//  
+//import MetalKit
 //
-//  Created by Noah Pikielny on 5/22/22.
+//extension BaseRenderer {
+//    public func createCopyPipeline(pixelFormat: MTLPixelFormat) -> CopyPipeline? {
+//        CopyPipeline(device: device, pixelFormat: pixelFormat)
+//    }
+//}
 //
-
-import MetalKit
-
-extension BaseRenderer {
-    public func createCopyPipeline(pixelFormat: MTLPixelFormat) -> CopyPipeline? {
-        CopyPipeline(device: device, pixelFormat: pixelFormat)
-    }
-}
-
-public struct CopyPipeline {
-    internal var pipeline: MTLRenderPipelineState
-    public init?(device: MTLDevice?, pixelFormat: MTLPixelFormat) {
-        do {
-            let library = try device?.makeLibrary(source: Self.libraryString, options: nil)
-            guard let vertexFunction = library?.makeFunction(name: "copyVertex"),
-                  let fragmentFunction = library?.makeFunction(name: "copyFragment") else {
-                      print("failed making shaders"); return nil
-                  }
-            let descriptor = MTLRenderPipelineDescriptor()
-            descriptor.vertexFunction = vertexFunction
-            descriptor.fragmentFunction = fragmentFunction
-            descriptor.sampleCount = 1
-            descriptor.colorAttachments[0].pixelFormat = pixelFormat
-            guard let pipeline = try device?.makeRenderPipelineState(descriptor: descriptor) else {
-                print("Failed making pipeline"); return nil
-            }
-            self.pipeline = pipeline
-        } catch {
-            print(error); return nil
-        }
-    }
-    
-    static let libraryString =
-"""
-// used to render the entire screen
-constant float2 cornerVerts[] = {
-    // top left
-    float2(-1, -1),
-    float2(-1,  1),
-    float2( 1, 1),
-    // bottom right
-    float2(-1, -1),
-    float2( 1,  1),
-    float2( 1, -1),
-};
-
-struct VertOut {
-    float4 position [[position]];
-    float2 uv;
-};
-
-vertex VertOut copyVertex(uint vid [[vertex_id]]) {
-    float2 vert = cornerVerts[vid];
-    
-    VertOut out;
-    out.position = float4(vert, 0, 1);
-    out.uv = vert * 0.5 + 0.5;
-    
-    return out;
-}
-
-fragment float4 copyFragment(VertOut in [[stage_in]],
-                             texture2d<float> image) {
-    float4 color = image.sample(sam, in.uv);
-    return color;
-}
-"""
-}
-
-extension MTLCommandBuffer {
-    public func copyTexture(pipeline: CopyPipeline, descriptor: MTLRenderPassDescriptor, drawable: MTLDrawable, from texture: MTLTexture) {
-        let renderEncoder = makeRenderCommandEncoder(descriptor: descriptor)
-        renderEncoder?.setRenderPipelineState(pipeline.pipeline)
-        renderEncoder?.setFragmentTexture(texture, index: 0)
-        renderEncoder?.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 6)
-        renderEncoder?.endEncoding()
-    }
-}
+//public struct CopyPipeline {
+//    internal var pipeline: MTLRenderPipelineState
+//    public init?(device: MTLDevice?, pixelFormat: MTLPixelFormat) {
+//        do {
+//            let library = try device?.makeLibrary(source: Self.libraryString, options: nil)
+//            guard let vertexFunction = library?.makeFunction(name: "copyVertex"),
+//                  let fragmentFunction = library?.makeFunction(name: "copyFragment") else {
+//                      print("failed making shaders"); return nil
+//                  }
+//            let descriptor = MTLRenderPipelineDescriptor()
+//            descriptor.vertexFunction = vertexFunction
+//            descriptor.fragmentFunction = fragmentFunction
+//            descriptor.sampleCount = 1
+//            descriptor.colorAttachments[0].pixelFormat = pixelFormat
+//            guard let pipeline = try device?.makeRenderPipelineState(descriptor: descriptor) else {
+//                print("Failed making pipeline"); return nil
+//            }
+//            self.pipeline = pipeline
+//        } catch {
+//            print(error); return nil
+//        }
+//    }
+//    
+//    static let libraryString =
+//"""
+//// used to render the entire screen
+//constant float2 cornerVerts[] = {
+//    // top left
+//    float2(-1, -1),
+//    float2(-1,  1),
+//    float2( 1, 1),
+//    // bottom right
+//    float2(-1, -1),
+//    float2( 1,  1),
+//    float2( 1, -1),
+//};
+//
+//struct VertOut {
+//    float4 position [[position]];
+//    float2 uv;
+//};
+//
+//vertex VertOut copyVertex(uint vid [[vertex_id]]) {
+//    float2 vert = cornerVerts[vid];
+//    
+//    VertOut out;
+//    out.position = float4(vert, 0, 1);
+//    out.uv = vert * 0.5 + 0.5;
+//    
+//    return out;
+//}
+//
+//fragment float4 copyFragment(VertOut in [[stage_in]],
+//                             texture2d<float> image) {
+//    float4 color = image.sample(sam, in.uv);
+//    return color;
+//}
+//"""
+//}
+//
+//extension MTLCommandBuffer {
+//    public func copyTexture(pipeline: CopyPipeline, descriptor: MTLRenderPassDescriptor, drawable: MTLDrawable, from texture: MTLTexture) {
+//        let renderEncoder = makeRenderCommandEncoder(descriptor: descriptor)
+//        renderEncoder?.setRenderPipelineState(pipeline.pipeline)
+//        renderEncoder?.setFragmentTexture(texture, index: 0)
+//        renderEncoder?.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 6)
+//        renderEncoder?.endEncoding()
+//    }
+//}
--- Sources/ShaderKit/PipelineRenderer.swift
+++ Sources/ShaderKit/PipelineRenderer.swift
@@ -1,54 +1,54 @@
+////
+////  File.swift
+////  
+////
+////  Created by Noah Pikielny on 5/18/22.
+////
 //
-//  File.swift
-//  
+//import MetalKit
+//import SwiftUI
 //
-//  Created by Noah Pikielny on 5/18/22.
+//public class PipelineRenderer: Renderer {
+//    private var pipelines: [ExecuteablePipeline]!
+//    
+//    public init?(
+//        device: MTLDevice? = MTLCreateSystemDefaultDevice(),
+//        threadCount: Int = 1,
+//        delegate: RendererDelegate?,
+//        pipelines: [Pipeline]
+//    ) throws {
+//        super.init(device: device, threadCount: threadCount, delegate: delegate)
+//        
+//        guard let pipelines = try pipelines.map(convertPipeline(_:)).unwrap() else { return nil }
+//        self.pipelines = pipelines
+//    }
+//    
+//    enum ExecuteablePipeline {
+//        case compute(MTLComputePipelineState)
+//        case render
+//    }
+//    
+//    private func convertPipeline(_ pipeline: Pipeline) throws -> ExecuteablePipeline? {
+//        switch pipeline {
+//            case .compute(let name, let constants):
+//                guard let computePipeline = try computePipeline(name: name, constants: constants) else { return nil }
+//                return .compute(computePipeline)
+//            case .render: return .render
+//        }
+//    }
+//    
+//    public enum Pipeline {
+//        case compute(name: String, _ constants: MTLFunctionConstantValues? = nil)
+//        case render
+//    }
+//    
+//    public func draw(in view: MTKView, with commandBuffer: MTLCommandBuffer?) {
+//        
+//    }
+//}
 //
-
-import MetalKit
-import SwiftUI
-
-public class PipelineRenderer: Renderer {
-    private var pipelines: [ExecuteablePipeline]!
-    
-    public init?(
-        device: MTLDevice? = MTLCreateSystemDefaultDevice(),
-        threadCount: Int = 1,
-        delegate: RendererDelegate?,
-        pipelines: [Pipeline]
-    ) throws {
-        super.init(device: device, threadCount: threadCount, delegate: delegate)
-        
-        guard let pipelines = try pipelines.map(convertPipeline(_:)).unwrap() else { return nil }
-        self.pipelines = pipelines
-    }
-    
-    enum ExecuteablePipeline {
-        case compute(MTLComputePipelineState)
-        case render
-    }
-    
-    private func convertPipeline(_ pipeline: Pipeline) throws -> ExecuteablePipeline? {
-        switch pipeline {
-            case .compute(let name, let constants):
-                guard let computePipeline = try computePipeline(name: name, constants: constants) else { return nil }
-                return .compute(computePipeline)
-            case .render: return .render
-        }
-    }
-    
-    public enum Pipeline {
-        case compute(name: String, _ constants: MTLFunctionConstantValues? = nil)
-        case render
-    }
-    
-    public func draw(in view: MTKView, with commandBuffer: MTLCommandBuffer?) {
-        
-    }
-}
-
-extension Array {
-    internal func unwrap<ElementOfResult>() -> [ElementOfResult]? where Self.Element == ElementOfResult? {
-        return contains { $0 == nil } ? nil : map { $0! }
-    }
-}
+//extension Array {
+//    internal func unwrap<ElementOfResult>() -> [ElementOfResult]? where Self.Element == ElementOfResult? {
+//        return contains { $0 == nil } ? nil : map { $0! }
+//    }
+//}

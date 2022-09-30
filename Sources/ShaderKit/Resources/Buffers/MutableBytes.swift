253fa2a2bea17d5e4e97163a4d0c781ec8364821
Noah Pikielny <np299@cornell.edu>
2022-09-30 16:00:11 -0400

    Mutable bytes

---
+++ Sources/ShaderKit/Resources/Buffers/MutableBytes.swift
@@ -0,0 +1,29 @@
+//
+//  MutableBytes.swift
+//  
+//
+//  Created by Noah Pikielny on 9/30/22.
+//
+
+import SwiftUI
+
+extension Bytes {
+    public init<T>(_ binding: UnsafePointer<T>, count: Int) where Encoder == MTLComputeCommandEncoder {
+        self.count = count
+        self.bytes = { encoder, index, _ in
+            encoder.setBytes(binding, length: MemoryLayout<T>.stride, index: index)
+        }
+    }
+    
+    public init<T>(_ binding: UnsafePointer<T>, count: Int) where Encoder == MTLRenderCommandEncoder {
+        self.count = count
+        self.bytes = { encoder, index, function in
+            switch function! {
+                case .fragment:
+                    encoder.setFragmentBytes(binding, length: MemoryLayout<T>.stride, index: index)
+                case .vertex:
+                    encoder.setVertexBytes(binding, length: MemoryLayout<T>.stride, index: index)
+            }
+        }
+    }
+}
